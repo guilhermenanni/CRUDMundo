6c5ed753adb473926d91dd6a6a1716a00aa52061
guilhermenanni <guilhermenanni2008@gmail.com>
2025-08-31 11:47:22 -0300

    reestruturaçao do projeto

--- site/footer.php
+++
@@ -1,4 +0,0 @@
-<div class="footer">
-    <p>obrigado por ver meus site</p>
-    <p>esse é na unha</p>
-</div>
--- site/header.php
+++
@@ -1,5 +0,0 @@
-<header>
-    <div class="headerholder">
-        <h1> CRUD MUNDO </h1>
-    </div>
-</header>
