bb1b9d90a8ca2b30a6afbb6c9db171829bb23b4a
guillherme <guilhermenanni2008@gmail.com>
2025-09-22 11:54:25 -0300

    estruturaçao inicial da aplicaçao web

---
+++ web/conn.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "db_mundo";
+
+$conex = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conex){
+    die ("falha na conexao, " .mysqli_connect_error());
+}
+
+echo "conexao realizada com sucesso";
+mysqli_select_db($conex $database);
+
+?>
